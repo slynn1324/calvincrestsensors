dfb74d9d06815c10e6a71bd09827347213be3e97
Stephen Lynn <slynn1324@gmail.com>
2026-05-13 18:47:34 -0500

    updated build to check parameter, added secrets example to remember how to configure

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
 GIT_REV=$(git rev-parse --short HEAD)
 GIT_DIRTY=$(git diff --quiet && git diff --cached --quiet || echo "-dirty")
 BUILD_TIME=$(date +"%Y%m%d%H%M%S")
