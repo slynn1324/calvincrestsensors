6736c742713201c6ffe0e92eb93683eeec5367fc
Stephen Lynn <slynn1324@gmail.com>
2026-05-12 21:54:24 -0500

    add build script that injects build_version

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+GIT_REV=$(git rev-parse --short HEAD)
+GIT_DIRTY=$(git diff --quiet && git diff --cached --quiet || echo "-dirty")
+BUILD_TIME=$(date +"%Y%m%d%H%M%S")
+VERSION="${GIT_REV}${GIT_DIRTY}.${BUILD_TIME}"
+
+echo "Building version: $VERSION"
+
+esphome -s build_version "$VERSION" compile node.yaml
